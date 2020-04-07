3a9151fa591274734d34f2d9284b58724ce85e08
hyx546 <love.1179612384@qq.com>
2020-04-07 19:54:11 +0800

    118.杨辉三角

---
+++ 118.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+  let arr=[];
+  for(let i =0;i<numRows;i++){
+    let nums =[]
+      for(let j=0;j<i+1;j++){
+          if(j==0 || j==i) nums.push(1);
+          else nums.push(arr[i-1][j-1] + arr[i-1][j])
+      }
+    arr.push(nums)
+  }
+  console.log(arr);
+};
+
+generate(7);
