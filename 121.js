948adebeae6dc41e0558c5f80610fb38a20730fa
hyx546 <love.1179612384@qq.com>
2020-04-08 13:51:48 +0800

    feat:121.买卖股票的最佳时机

---
+++ 121.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let max =0;
+  for(let i =0;i<prices.length;i++){
+      for(let j =i+1;j<prices.length;j++) {
+          if(prices[i]<prices[j]){
+              max =(prices[j]-prices[i])>max? prices[j]-prices[i]:max;
+          }
+      }
+  }
+  return max
+};
