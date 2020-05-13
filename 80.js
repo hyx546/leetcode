05ae3487db376637d6c5ef69eeb854c049c2ea50
hyx546 <love.1179612384@qq.com>
2020-05-13 09:27:30 +0800

    80.删除排序数组中的重复项II

---
+++ 80.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  for(let i=2;i<nums.length;i++) {
+      if(nums[i-2] === nums[i]) {
+          nums.splice(i,1);
+          i--;
+      }
+  }
+  return nums.length
+};
