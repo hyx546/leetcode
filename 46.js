f96c6ea8321cf2eb677b46474e0d9d0b5248d5dd
hyx546 <love.1179612384@qq.com>
2020-06-27 21:21:28 +0800

    46.全排列

---
+++ 46.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  let res = []
+  dfs([])
+  function dfs(path) {
+    if (path.length === nums.length) {
+      res.push(path.slice())
+    }
+    for (const num of nums) {
+      if (path.includes(num)) continue
+      path.push(num)
+      dfs(path)
+      path.pop()
+    }
+  }
+  return res
+};
