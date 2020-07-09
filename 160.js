e757dae896dc05f9db4ec9ba6dee652309280dc6
hyx546 <love.1179612384@qq.com>
2020-07-09 09:46:38 +0800

    160. 相交链表

---
+++ 160.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+  while(headA){
+      headA.flag = true;
+      headA = headA.next;
+  }
+
+  while(headB){
+      if(headB.flag) return headB;
+      headB = headB.next;
+  }
+  return null
+};
