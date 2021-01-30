933d5855216fa3dfe270f5b178aa509f75787938
hyx546 <love.1179612384@qq.com>
2021-01-30 22:48:42 +0800

    98.验证二叉树

--- 98.js
+++ 98.js
@@ -10,6 +10,8 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+
+//  中序遍历递增
 var isValidBST = function (root) {
   if (!root) return true;
   const res = [];
@@ -31,4 +33,32 @@ var isValidBST = function (root) {
 
   return flag
 
+};
+
+
+
+// 左子树比根小， 右子树比根大
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var helper = (node,low,high) => {
+  if(!node) return true;
+  if(node.val <= low || node.val >=high){
+      return false;
+  }
+  return helper(node.left,low,node.val) && helper(node.right,node.val,high);
+
+}
+var isValidBST = function(root) {
+ 
+  return helper(root,-Infinity,Infinity)
 };
