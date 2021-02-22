5367f31f6233b3056cfcddfb62ae6543e102cf8f
hyx546 <love.1179612384@qq.com>
2021-02-22 20:25:49 +0800

    剑指 Offer 48. 最长不含重复字符的子字符串/哈希方式

--- 面试题/48.js
+++ 面试题/48.js
@@ -1,18 +1,34 @@
+// res
+// var lengthOfLongestSubstring = function(s) {
+//   // if(s.length<=1) return s.length;
+//   let max=0,res='';
+//   for(let i=0;i<s.length;i++){
+//     if(!res.includes(s[i])){
+//       res +=s[i];
+//       max = Math.max(max,res.length)
+//     }
+//     else{
+//       res = res.slice(res.indexOf(s[i])+1);
+//       res +=s[i]
+//     }
+//   }
+//   console.log(max);
+//   // return max
+// };
+
+// map
 var lengthOfLongestSubstring = function(s) {
-  // if(s.length<=1) return s.length;
-  let max=0,res='';
-  for(let i=0;i<s.length;i++){
-    if(!res.includes(s[i])){
-      res +=s[i];
-      max = Math.max(max,res.length)
+  let i=-1;
+  const map = new Map();
+  let res = 0;
+  for (let j = 0; j < s.length; j++) {
+    if(map.has(s[j])){
+      i = Math.max(i,map.get(s[j]))
     }
-    else{
-      res = res.slice(res.indexOf(s[i])+1);
-      res +=s[i]
-    }
+    res = Math.max(res,j-i);
+    map.set(s[j],j);
   }
-  console.log(max);
-  // return max
+  console.log(res);
 };
 
 lengthOfLongestSubstring("bbtablud")
