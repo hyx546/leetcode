c28f746ee037f5db6e5d2de25231c497e51eed8e
hyx546 <love.1179612384@qq.com>
2021-02-24 12:22:10 +0800

    [二维矩阵]:832. 翻转图像

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
-const arr = [1, 2, 3, 4, 5]
-const isAllNum = arr.every(item => typeof item === 'number')
-console.log(isAllNum);
+var mergeAlternately = function(word1, word2) {
+  const arr1 = word1.split('');
+  const arr2 = word2.split('');
+  let s ="";
+  while(arr1.length && arr2.length){
+      s+=arr1.shift();
+      s+=arr2.shift();
+  }
+  console.log(arr2);
+  if(arr1.length) s+=arr1.join('');
+  if(arr2.length) s+=arr2.join('')
+  console.log(s);
+  // return s
+};
+
+mergeAlternately("ab","pqrs")
