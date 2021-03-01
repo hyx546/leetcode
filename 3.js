23a10a4853d91e55f9b6c632107f7c97644c91d7
hyx546 <love.1179612384@qq.com>
2021-03-01 14:08:39 +0800

    [数组]：3. 无重复字符的最长子串

---
+++ 3.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// var lengthOfLongestSubstring = function (s) {
+//   let left = right = max = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     if (map.has(s[i]) && map.get(s[i]) >= left) {
+//       left = map.get(s[i]) + 1
+//     }
+//     max = Math.max(max, i - left + 1)
+//     map.set(s[i], i);
+//   }
+//   return max
+// };
+
+var lengthOfLongestSubstring = function (s) {
+  let left = max = index = 0;
+  for (let i = 0; i < s.length; i++) {
+    index = s.slice(left,i).indexOf(s[i])
+    if(index == -1){
+      max = Math.max(max, i - left + 1)
+    }
+    else{
+      console.log(index);
+      console.log('---pre',left);
+      left += index+1;
+      console.log('----next',left);
+    }
+  }
+  return max
+};
+
+console.log(lengthOfLongestSubstring('bbtablud'));
