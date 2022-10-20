e154ce0c56c7add4337cc45ccbeefc1cc00476a5
hyx546 <love.1179612384@qq.com>
2022-10-20 19:25:01 +0800

    42. 连续子数组的最大和

--- 面试题/42.js
+++ 面试题/42.js
@@ -2,12 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
   let sum = cur = prev = nums[0];
-  for(let i =1;i<nums.length;i++){
-      cur = prev>=0?prev+nums[i]:nums[i];
-      prev = cur;
-      sum = Math.max(sum,cur)
+  for (let i = 1; i < nums.length; i++) {
+    cur = prev >= 0 ? prev + nums[i] : nums[i];
+    prev = cur;
+    sum = Math.max(sum, cur);
   }
-  return sum
+  return sum;
+};
+
+
+var maxSubArray = function (nums) {
+  let sum = nums[0], pre = 0;
+  for (let i = 0; i < nums.length; i++) {
+    pre = Math.max(pre + nums[i], nums[i]);
+    sum = Math.max(pre, sum);
+  }
+  return sum;
 };
