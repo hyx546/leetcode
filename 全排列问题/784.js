b80bbdb3ef8b925da693de51ec788d7e37906297
hyx546 <love.1179612384@qq.com>
2022-10-30 20:39:35 +0800

    784. 字母大小写全排列

---
+++ 全排列问题/784.js
@@ -0,0 +1,44 @@
+const { use } = require('express/lib/application');
+
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var letterCasePermutation = function (s) {
+    let res = [];
+
+    const isChar = (char) => {
+        const isFlag = char >= 'A' && char <= 'Z';
+        const isFlag2 = char >= 'a' && char <= 'z';
+        return isFlag || isFlag2;
+    };
+
+    const translateChar = (char) => {
+        const isFlag = char >= 'A' && char <= 'Z';
+        const isFlag2 = char >= 'a' && char <= 'z';
+        if (isFlag) {
+            return char.toLowerCase();
+        }
+        else if (isFlag2) {
+            return char.toUpperCase();
+        }
+    };
+
+    const dns = (start, n, cur) => {
+        if (start === n) {
+            res.push(cur.slice());
+            return;
+        }
+
+        dns(start + 1, n, cur + s[start]);
+        if (isChar(s[start])) {
+            dns(start + 1, n, cur + translateChar(s[start]));
+        }
+    };
+
+    dns(0, s.length, '');
+    return res;
+};
+
+
+console.log(letterCasePermutation(s = "3z4"));
