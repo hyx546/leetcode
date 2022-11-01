a458a644061b61de3b79ac633a6161598dd00cc0
hyx546 <love.1179612384@qq.com>
2022-11-01 15:17:31 +0800

    64. 最小路径和

--- 56.js
+++ 56.js
@@ -2,19 +2,38 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function(intervals) {
-  if(intervals.length === 0) return []
-intervals = intervals.sort((a,b) => a[0]-b[0])
-let arr =[intervals[0]];
-for (let i = 1; i < intervals.length; i++) {
-    if(intervals[i][0] > arr[arr.length-1][1]){
-      arr.push(intervals[i])
+// var merge = function(intervals) {
+//   if(intervals.length === 0) return []
+// intervals = intervals.sort((a,b) => a[0]-b[0])
+// let arr =[intervals[0]];
+// for (let i = 1; i < intervals.length; i++) {
+//     if(intervals[i][0] > arr[arr.length-1][1]){
+//       arr.push(intervals[i])
+//     }
+//    else{
+//       if(intervals[i][1] > arr[arr.length-1][1]){
+//         arr[arr.length-1][1]=intervals[i][1]
+//       }
+//     }
+// }
+// return arr
+// };
+
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const arr = [intervals[0]];
+
+  for (let i = 1; i < intervals.length; i++) {
+    const item = arr[arr.length - 1];
+    if (intervals[i][0] > item[1]) {
+      arr.push(intervals[i]);
+    } else {
+      arr[arr.length - 1][1] = Math.max(intervals[i][1], item[1]);
     }
-   else{
-      if(intervals[i][1] > arr[arr.length-1][1]){
-        arr[arr.length-1][1]=intervals[i][1]
-      }
-    }
-}
-return arr
+  }
+  return arr;
 };
+
+console.log('====================================');
+console.log(merge([[1, 3], [2, 6], [8, 10], [15, 18]]));
+console.log('====================================');
