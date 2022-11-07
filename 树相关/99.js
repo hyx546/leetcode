f8c10de8a5c6f7449b182b407b86ca0ff2f7293f
hyx546 <love.1179612384@qq.com>
2022-11-08 00:05:11 +0800

    99. 恢复二叉搜索树;

--- 树相关/99.js
+++ 树相关/99.js
@@ -12,25 +12,52 @@
  * @return {void} Do not return anything, modify root in-place instead.
  */
 var recoverTree = function (root) {
-    let prev = new TreeNode(-Infinity);
-    let a1 = a2 = null;
+    // let prev = new TreeNode(-Infinity);
+    // let a1 = a2 = null;
+    // const dfs = (root) => {
+    //     if (!root) return;
+    //     dfs(root.left);
+    //     if (prev.val > root.val) {
+    //         a2 = root;
+    //         if (!a1) {
+    //             a1 = prev;
+    //         }
+    //     }
+    //     prev = root;
+    //     dfs(root.right);
+    // };
+    // dfs(root);
+
+    // if (a1 && a2) {
+    //     const val = a1.val;
+    //     a1.val = a2.val;
+    //     a2.val = val;
+    // }
+
+
+    const arr = [];
     const dfs = (root) => {
         if (!root) return;
         dfs(root.left);
-        if (prev.val > root.val) {
-            a2 = root;
-            if (!a1) {
-                a1 = prev;
-            }
-        }
-        prev = root;
+        arr.push(root);
         dfs(root.right);
     };
     dfs(root);
+
+    let a1 = a2 = null;
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i].val > arr[i + 1].val) {
+            a2 = arr[i + 1];
+            if (!a1) {
+                a1 = arr[i];
+            }
+        }
+    }
 
     if (a1 && a2) {
         const val = a1.val;
         a1.val = a2.val;
         a2.val = val;
     }
+    return root;
 };
