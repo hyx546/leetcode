e2188b846eb6b1aea13be7ba60ab290f2975c82d
hyx546 <love.1179612384@qq.com>
2022-11-28 14:42:06 +0800

    538. 把二叉搜索树转换为累加树

--- 树相关/538.js
+++ 树相关/538.js
@@ -38,3 +38,18 @@ var convertBST = function (root) {
     newDfs(root);
     return root;
 };
+
+
+// 中序递归反向
+var convertBST = function (root) {
+    let sum = 0;
+    const dfs = (root) => {
+        if (!root) return;
+        dfs(root.right);
+        sum += root.val;
+        root.val = sum;
+        dfs(root.left);
+    };
+    dfs(root);
+    return root;
+};
