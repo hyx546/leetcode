36f9e530f47ec596ff1e7a0125601dad762e7253
hyx546 <love.1179612384@qq.com>
2022-11-29 23:39:29 +0800

    test git

--- test.js
+++ test.js
@@ -1,16 +1,1 @@
-var mergeAlternately = function(word1, word2) {
-  const arr1 = word1.split('');
-  const arr2 = word2.split('');
-  let s ="";
-  while(arr1.length && arr2.length){
-      s+=arr1.shift();
-      s+=arr2.shift();
-  }
-  console.log(arr2);
-  if(arr1.length) s+=arr1.join('');
-  if(arr2.length) s+=arr2.join('')
-  console.log(s);
-  // return s
-};
-
-mergeAlternately("ab","pqrs")
+console.log('test git clone');
